adaf0b6ec6deafb4334c397e99d248d8118ec3c0
galandras13 <galandras13@gmail.com>
2022-02-18 01:18:07 +0100

    More refactor for offline access

--- static/js/main.js
+++ static/js/main.js
@@ -16,36 +16,18 @@ export async function reset () {
 }
 
 const registerSW = () => {
-    let boardIds = [];
-    let cardURLs = [];
-
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', function () {
-            navigator.serviceWorker.register('../sw.js', {}).then(function () {
+            navigator.serviceWorker.register('../sw.js').then(function () {
                 return navigator.serviceWorker.ready;
             }).then(function (registration) {
                 console.log('Registration successful, scope is:', registration.scope);
-
-                getIds(boardIds, cardURLs).then(function () {
-                    registration.active.postMessage(JSON.stringify(cardURLs));
-                });
             }).catch(function (error) {
-                console.log('Service worker registration failed, error:', error);
+                console.error('Service worker registration failed, error:', error);
             });
         });
     }
 }
 
-const getIds = async (boardIds, cardURLs) => {
-    const boards = document.querySelectorAll(".board");
-    for (const board of boards) {
-        boardIds.push(board.dataset.boardId);
-    }
-    for (const boardId of boardIds) {
-        cardURLs.push(
-            `/api/boards/${boardId}/cards/`
-        );
-    }
-}
-
-await init().then(registerSW);
+registerSW();
+await init();
